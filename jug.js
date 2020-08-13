4e3b37d4d635ece853b118315e6405a9f4eb2478
Zack Guard <zacharyguard@gmail.com>
2020-08-14 01:37:35 +0800

    Handle crlf properly

--- jug.js
+++ jug.js
@@ -4,6 +4,16 @@ const p = require("./parser/parser");
 
 const trailingWhitespacePat = /(\r|\t| )*$/;
 
+function startsWithNewline(text) {
+    return text[0] === "\n" || text.substring(0, 2) === "\r\n";
+}
+
+function stripLeadingNewline(text) {
+    let start = 1;
+    if (text.substring(0, 2) === "\r\n") start = 2;
+    return text.substring(start);
+}
+
 function jug(doc, config) {
     const vars = {
         indentLevel: 0,
@@ -17,11 +27,12 @@ function jug(doc, config) {
     const parsed = p.parse(doc);
     let prevText, prevItem;
     for (const item of parsed) {
+        // console.log(item);
         const [kind, text] = item;
         if (kind === 0) { // plaintext
             let textProcessed = text;
-            if (prevItem && prevItem[0] === 1 && text[0] === "\n") {
-                textProcessed = textProcessed.substring(1);
+            if (prevItem && prevItem[0] === 1 && startsWithNewline(text)) {
+                textProcessed = stripLeadingNewline(textProcessed);
             }
             prevText = textProcessed;
             vars.outs.push(textProcessed);
